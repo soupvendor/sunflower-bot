8d142ea339f0168c8be21e4c491ba51ff00b5d51
Nick <nickseldner@gmail.com>
2021-06-30 21:00:02 -0400

    updated bot.py

--- bot.py
+++ bot.py
@@ -13,4 +13,18 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print("Bot is ready.")
 
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server!")
+
+@client.event
+async def on_member_remove(member):
+    
+
+
+
+
+
 client.run(TOKEN)
+
+
